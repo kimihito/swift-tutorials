5809ab151d273c6a8760153c6559d7c14c5e83c5
kimihito <tatsurotamashiro@gmail.com>
2016-08-22 18:45:45 +0900

    project10

---
+++ project10/project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  project10
+//
+//  Created by kimihito on 2016/08/22.
+//  Copyright © 2016年 minotake. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
