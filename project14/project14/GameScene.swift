723f3406dc4697f8407367ddff1f309d6e9529ad
kimihito <tatsurotamashiro@gmail.com>
2016-08-24 13:46:37 +0900

    Project14

--- project14/project14/GameScene.swift
+++ project14/project14/GameScene.swift
@@ -6,6 +6,7 @@
 //  Copyright (c) 2016年 minotake. All rights reserved.
 //
 
+import GameplayKit
 import SpriteKit
 
 class GameScene: SKScene {
@@ -16,10 +17,11 @@ class GameScene: SKScene {
         }
     }
     
+    var numRounds = 0
     var slots = [WhackSlot]()
+    var popupTime = 0.85
     
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
         let background = SKSpriteNode(imageNamed: "whackBackground")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .Replace
@@ -38,25 +40,43 @@ class GameScene: SKScene {
         for i in 0 ..< 5 { createSlotAt(CGPoint(x: 100 + (i * 170), y: 410)) }
         for i in 0 ..< 4 { createSlotAt(CGPoint(x: 180 + (i * 170), y: 140)) }
         
+        RunAfterDelay(1) { [unowned self] in
+            self.createEmemy()
+        }
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
+        if let touch = touches.first {
             let location = touch.locationInNode(self)
+            let nodes = nodesAtPoint(location)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            for node in nodes {
+                if node.name == "charFriend" {
+                    // they shouldn't have whacked this penguin
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.visible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.hit()
+                    score -= 5
+                    
+                    runAction(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+                } else if node.name == "charEnemy" {
+                    // they should have whacked this one
+                    let whackSlot = node.parent!.parent as! WhackSlot
+                    if !whackSlot.visible { continue }
+                    if whackSlot.isHit { continue }
+                    
+                    whackSlot.charNode.xScale = 0.85
+                    whackSlot.charNode.yScale = 0.85
+                    
+                    whackSlot.hit()
+                    score += 1
+                    
+                    runAction(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+                }
+            }
         }
     }
    
@@ -69,5 +89,39 @@ class GameScene: SKScene {
         slot.configureAtPosition(pos)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEmemy() {
+        numRounds += 1
+        if numRounds >= 30 {
+            for slot in slots {
+                slot.hide()
+            }
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 512, y: 384)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            
+            return
+        }
+        
+
+        
+        popupTime *= 0.991
+        
+        slots = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(slots) as! [WhackSlot]
+        slots[0].show(hideTime: popupTime)
+        
+        if RandomInt(min: 0, max: 12) > 4 { slots[1].show(hideTime: popupTime) }
+        if RandomInt(min: 0, max: 12) > 8 { slots[2].show(hideTime: popupTime) }
+        if RandomInt(min: 0, max: 12) > 10 { slots[3].show(hideTime: popupTime) }
+        if RandomInt(min: 0, max: 12) > 11 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        
+        RunAfterDelay(RandomDouble(min: minDelay, max: maxDelay)) { [unowned self] in
+            self.createEmemy()
+        }
     }
 }
